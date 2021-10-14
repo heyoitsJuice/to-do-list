07a366bae70a7c2c99cce970f0b887cf3c51ccae
Justin Clenista <justinalvin98@gmail.com>
2021-10-13 23:49:29 -0700

    feat: implemented basic styling to show kanban board functionality

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,9 +1,41 @@
 import React, { useState } from 'react'
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import _ from 'lodash';
+import { makeStyles } from '@mui/styles'
+import { Button, Typography, Grid, TextField } from '@mui/material'
 import { v4 } from 'uuid';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    // display: 'flex',
+    // width: '100%',
+    // height: '100%',
+    // justifyContent: 'space-evenly'
+  },
+  column: {
+    width: '20%',
+  },
+  droppable: {
+    width: '100%',
+    minHeight: '500px',
+    backgroundColor: '#e0e0e0',
+    flexDirection: 'column',
+    display: 'flex',
+    borderRadius: '7px',
+    padding: '10px'
+  },
+  item: {
+    backgroundColor: 'white',
+    color: 'black',
+    border: '1px solid black',
+    padding: '10px',
+    borderRadius: '7px',
+    marginBottom: '10px'
+  }
+}))
+
 function App() {
+  const classes = useStyles()
   const [text, setText] = useState("")
   const [ state, setState ] = useState({
     "todo": {
@@ -50,10 +82,10 @@ function App() {
       return {
         ...prev,
         todo: {
-          title: "title",
+          title: "Todo",
           items: [
             {
-              id:v4(), 
+              id: v4(), 
               name: text
             }, 
             ...prev.todo.items
@@ -66,48 +98,56 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <div>
-        <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
-        <button onClick={addItem}>Add</button>
-      </div>
-      <DragDropContext onDragEnd={handleDragEnd}>
-        {_.map(state, (data, key) => {
-          return (
-            <div key={key}>
-              <div>{data.title}</div>
-              <Droppable droppableId={key}>
-                {(provided) => {
-                  return (
-                    <div 
-                      ref={provided.innerRef}
-                      {...provided.droppableProps}>
-                      {data.items.map((el, index) => {
-                        return(
-                          <Draggable key={el.id} index={index} draggableId={el.id}>
-                            {(provided) => {
+    <Grid className={classes.root}>
+      <Grid container alignItems="center" direction="column">
+        <div>
+          <TextField type="text" label="Add a task..." variant="standard" value={text} onChange={(e) => setText(e.target.value)} />
+          <Button onClick={addItem}>Add</Button>
+        </div>
+        <Grid container>
+          <DragDropContext onDragEnd={handleDragEnd}>
+            <Grid container direction="row" justifyContent="space-around">
+              {_.map(state, (data, key) => {
+                return (
+                  <div key={key} variant="h1" className={classes.column}>
+                    <Typography variant="h4">{data.title}</Typography>
+                    <Droppable droppableId={key}>
+                      {(provided) => {
+                        return (
+                          <div 
+                            className={classes.droppable}
+                            ref={provided.innerRef}
+                            {...provided.droppableProps}>
+                            {data.items.map((el, index) => {
                               return(
-                                <div
-                                  ref={provided.innerRef}
-                                  {...provided.draggableProps}
-                                  {...provided.dragHandleProps}
-                                >
-                                  {el.name}
-                                </div>
+                                <Draggable key={el.id} index={index} draggableId={el.id}>
+                                  {(provided) => {
+                                    return(
+                                      <div
+                                      className={classes.item}
+                                        ref={provided.innerRef}
+                                        {...provided.draggableProps}
+                                        {...provided.dragHandleProps}
+                                      >
+                                        {el.name}
+                                      </div>
+                                    )
+                                  }}
+                                </Draggable>
                               )
-                            }}
-                          </Draggable>
+                            })}
+                          </div>
                         )
-                      })}
-                    </div>
-                  )
-                }}
-              </Droppable>
-            </div>
-          )
-        })}
-      </DragDropContext>
-    </div>
+                      }}
+                    </Droppable>
+                  </div>
+                )
+              })}
+            </Grid>
+          </DragDropContext>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
 
